226d403f0ec6ae0cc03955711db774b1c588123a
Mihail Pankratov <mpankratov@rzdit.ru>
2023-01-13 01:39:40 +0300

    final commit

---
+++ app/src/main/java/com/android/uraall/pizzarecipes/RecipeActivity.java
@@ -0,0 +1,25 @@
+package com.android.uraall.pizzarecipes;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RecipeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe);
+
+        TextView title = findViewById(R.id.titleTextView);
+        TextView recipe = findViewById(R.id.recipeTextView);
+
+        Intent intent = getIntent();
+        if (intent != null) {
+            title.setText(intent.getStringExtra("title"));
+            recipe.setText(intent.getStringExtra("recipe"));
+        }
+    }
+}
